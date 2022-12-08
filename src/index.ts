8a4048e6ceafb5bf5c8f7195b2ec92eefcf99fe9
Karl Holub <holub008@umn.edu>
2022-12-08 03:40:57 -0600

    hate and despair

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight";
-import {Completion, autocompletion, CompletionContext} from "@codemirror/autocomplete"
+import { parser } from "./syntax.grammar"
+import { LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language"
+import { styleTags, tags as t } from "@lezer/highlight";
+import { completeFromList } from "@codemirror/autocomplete"
 
 export const boolLanguage = LRLanguage.define({
   parser: parser.configure({
@@ -29,28 +29,12 @@ export const boolLanguage = LRLanguage.define({
   }),
 });
 
-/**
- * Customizable autocomplete function for lang-bool
- * @param completeFromListParameter Completion[]
- * @returns CodeMirror autocomplete
- */
-export const boolCompletion = (completeFromListParameter: Completion[]=[]) => {
-  function getCompletionsFromList(list: Completion[]) {
-    let options = list.map(o => typeof o == "string" ? { label: o } : o);
-    let [span, match] = [/\w*$/, /\w+$/];
-    return (context: CompletionContext) => {
-      let token = context.matchBefore(match);
-      return token || context.explicit ? { from: token ? token.from : context.pos, options, span } : null;
-    };
-  }
-  return autocompletion({
-    override: [getCompletionsFromList(completeFromListParameter.concat([
+export function bool() {
+  return new LanguageSupport(boolLanguage, boolLanguage.data.of({
+    autocomplete: completeFromList([
       {label: "AND", type: "keyword"},
       {label: "OR", type: "keyword"},
       {label: "NOT", type: "keyword"},
-    ]))]
-  });
-}
-export function bool() {
-  return new LanguageSupport(boolLanguage)
+    ]),
+  }))
 }
